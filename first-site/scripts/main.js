e3267d592f3e96dc4f8a0ae7791e951868ea78bf
sugenes <30437452+sugenes@users.noreply.github.com>
2017-07-25 16:07:14 +0300

    Add files via upload

---
+++ first-site/scripts/main.js
@@ -0,0 +1,33 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    
+    if(mySrc === 'images/cover.jpg') {
+      myImage.setAttribute ('src','images/baphomet.jpg');
+    } 
+    else {
+      myImage.setAttribute ('src','images/cover.jpg');
+    }
+}
+var
+myButton = document.querySelector('button');
+var myHeading = document.querySelector('h2');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'SICP is cool, ' + myName;
+}
+    
+if(!localStorage.getItem('name')) {
+  setUserName();
+} 
+else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'SICP is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
